cdaa1d60d46510b659d54b8d491fb9659a65b1ea
00andobe <00andobe@skola.boras.se>
2023-01-11 17:15:40 +0100

    Ny print function som printar ut alla scientists same skapar en EventListener på varje scientist som printas.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ var EvilScientists = /** @class */ (function () {
 }());
 var scientists = new EvilScientists().scientists; // skapar en ny instance och kör koden i constructor o sedan tar property och sparar det i den här variabeln. 
 console.log(scientists);
+printScientist(); // Printar ut alla scientists i början samt lägger ut en eventlistener för när man trycker på en scientists namn, vilken man trycker på kommer koden ihåg automatiskt pga forEach loopen.
+function printScientist() {
+    scientistList.innerHTML = "";
+    scientists.forEach(function (scientist) {
+        var scientistName = scientist.name;
+        var scientistDiv = document.createElement("div");
+        scientistDiv.innerHTML = scientistName;
+        scientistList.appendChild(scientistDiv);
+        scientistDiv.addEventListener("click", function () {
+            var selectedSection = document.getElementById("selected-Scientist");
+            selectedSection.innerHTML = "<p>Name: ".concat(scientist.name, "</p>\n            <p>Henchmen: ").concat(scientist.henchmen, "</p>\n            <p>Description: ").concat(scientist.description, "</p>");
+        });
+    });
+}
--- index.ts
+++ index.ts
@@ -12,3 +12,20 @@ constructor() {
 }
 const scientists = new EvilScientists().scientists; // skapar en ny instance och kör koden i constructor o sedan tar property och sparar det i den här variabeln. 
 console.log(scientists);
+
+printScientist() // Printar ut alla scientists i början samt lägger ut en eventlistener för när man trycker på en scientists namn, vilken man trycker på kommer koden ihåg automatiskt pga forEach loopen.
+function printScientist() {
+  scientistList.innerHTML = "";
+  scientists.forEach((scientist) => {
+    const scientistName = scientist.name;
+    const scientistDiv = document.createElement("div");
+    scientistDiv.innerHTML = scientistName;
+    scientistList.appendChild(scientistDiv);
+    scientistDiv.addEventListener("click", () => {
+        const selectedSection = document.getElementById("selected-Scientist") as HTMLDivElement;
+        selectedSection.innerHTML = `<p>Name: ${scientist.name}</p>
+            <p>Henchmen: ${scientist.henchmen}</p>
+            <p>Description: ${scientist.description}</p>`;
+    });
+  });
+}
