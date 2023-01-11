104123bcb283ad18d8f0663ef8f3ef84a98eda92
00andobe <121859409+00andobe@users.noreply.github.com>
2023-01-11 17:28:00 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var scientistList = document.getElementById("scientist-List");
 var EvilScientists = /** @class */ (function () {
     function EvilScientists() {
         this.scientists = [
-            { name: "Dr. Anton", henchmen: 2, description: "Wack" },
+            { name: "Dr. Anton", henchmen: 2, description: "Super cool guy" },
             { name: "Dr. Max", henchmen: 8, description: "Cool guy" },
             { name: "Dr. Andre", henchmen: 52, description: "Cool guy" }
         ];
